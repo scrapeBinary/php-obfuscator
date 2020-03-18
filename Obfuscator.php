30791b6b5380962a2ba3a07471a4438fcd2a7221
ANAMPEDIA <62225978+namdevel@users.noreply.github.com>
2020-03-18 16:36:09 +0700

    Update Obfuscator.php

--- Obfuscator.php
+++ Obfuscator.php
@@ -12,6 +12,8 @@ class Obfuscator
     protected $_signature;
     protected $_local;
 	protected $_exploder;
+	protected $_key;
+	protected $_secret;
 	
     public function __construct($file)
     {
@@ -19,13 +21,15 @@ class Obfuscator
         $this->_signature = sha1(microtime());
         $this->_local = "_NAMDEVEL_" . $this->md5rand(8);
 		$this->_exploder = substr(base64_encode(md5(microtime())), 0,5);
+		$this->_key = sha1(time());
+		$this->_secret = md5(time());
     }
 
     public function run()
     {
         $content = $this->e($this->get());
 		$md1 = $this->md5rand(5);$md2 = $this->md5rand(5);$md3 = $this->md5rand(5);$md4 = $this->md5rand(7);$md5 = $this->md5rand(7);$md6 = $this->md5rand(7);
-		$layer_1 = 'new Namdevel;class Namdevel{public function __construct(){if(!preg_match("#'.$this->_signature.'#",file_get_contents('.$this->_local.'))){exit("NAMDEVEL - Signature verification failed, visit https://github.com/namdevel");}if(count(file('.$this->_local.'))!=9){exit("NAMDEVEL - File corrupt, visit https://github.com/namdevel");}if(!preg_match("#Obfuscation provided by NAMDEVEL|https://github.com/namdevel/#",file_get_contents('.$this->_local.'))){exit("NAMDEVEL - File corrupt, visit https://github.com/namdevel");}$this->load('.$this->_local.');}protected function load($s){if(!$s){return false;}$c=explode("Namdevel::",file_get_contents($s));$sx=gzinflate(base64_decode(str_replace("_$\_","",$c[1])));$x=gzinflate(hex2bin(json_decode($sx,true)["alva"]));$k=sha1("NDVL::".strtoupper(md5("NDVL")));$sl=strlen($x);$kl=strlen($k);$j=0;$dt="";for($i=0;$i<$sl;$i+=2){$os=hexdec(base_convert(strrev(substr($x,$i,2)),36,16));if($j==$kl){$j=0;}$ok=ord(substr($k,$j,1));$j++;$dt.=chr($os-$ok);}eval($dt);}}';
+		$layer_1 = 'new Namdevel;class Namdevel{public function __construct(){if(!preg_match("#'.$this->_signature.'#",file_get_contents('.$this->_local.'))){exit("NAMDEVEL - Signature verification failed, visit https://github.com/namdevel");}if(count(file('.$this->_local.'))!=9){exit("NAMDEVEL - File corrupt, visit https://github.com/namdevel");}if(!preg_match("#Obfuscation provided by NAMDEVEL|https://github.com/namdevel/#",file_get_contents('.$this->_local.'))){exit("NAMDEVEL - File corrupt, visit https://github.com/namdevel");}$this->load('.$this->_local.');}protected function load($s){if(!$s){return false;}$c=explode("Namdevel::",file_get_contents($s));$sx=gzinflate(base64_decode(str_replace("_$\_","",$c[1])));$x=gzinflate(hex2bin(json_decode($sx,true)["alva"]));$k=sha1("'.$this->_key.'".strtoupper(md5("'.$this->_secret.'")));$sl=strlen($x);$kl=strlen($k);$j=0;$dt="";for($i=0;$i<$sl;$i+=2){$os=hexdec(base_convert(strrev(substr($x,$i,2)),36,16));if($j==$kl){$j=0;}$ok=ord(substr($k,$j,1));$j++;$dt.=chr($os-$ok);}eval($dt);}}';
 		$layer_2 = 'call_user_func("run_reflection");function run_reflection(){return eval(strrev(gzinflate(base64_decode(explode("'.$this->_exploder.'",preg_replace("_@_","",file_get_contents('.$this->_local.')))[1]))));}';
 		$layer_3 = $this->s('$NAMDEVEL_'.$md1.'=function($NAMDEVEL_'.$md2.',$NAMDEVEL_'.$md3.'){@eval($NAMDEVEL_'.$md2.'(base64_decode($NAMDEVEL_'.$md3.')));};$NAMDEVEL_'.$md1.'(hex2bin("'.bin2hex('gzinflate').'"),explode(strrev("::levedmaN"),@file_get_contents('.$this->_local.'))[2]);');
 		$layer_4 = base64_encode('$NAMDEVEL_'.$md4.'=function($str="'.str_replace(array('"', '$'), array('\"', '\$'), $layer_3).'"){$ky=str_replace(chr(32),"","12345678");if(strlen($ky)<8)exit();$kl=strlen($ky)<32?strlen($ky):32;$k=array();for($i=0;$i<$kl;$i++){$k[$i]=ord($ky{$i})&0x1F;}$j=0;for($i=0;$i<strlen($str);$i++){$e=ord($str{$i});$str{$i}=$e&0xE0?chr($e^$k[$j]):chr($e);$j++;$j=$j==$kl?0:$j;}eval($str);};$NAMDEVEL_'.$md4.'();');
@@ -79,7 +83,7 @@ const '.$this->_local.'=__FILE__;$NAMDEVEL_'.$md5.'="'.$this->h('base64_decode')
 
     protected function e($s)
     {
-        $k = sha1('NDVL::' . strtoupper(md5('NDVL')));
+        $k = sha1($this->_key . strtoupper(md5($this->_secret)));
         if (!$s)
         {
             return false;
